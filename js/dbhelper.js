1317f5a97eb5b75bf97c9c45c2f52c1392b79e44
Amal Krishnan <amal.aug95@gmail.com>
2018-07-17 12:22:03 +0530

    changed server url for json fetch

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     // const port = 8000 // Change this to your server port
-    return `http://krishamal.github.io/mws-restaurant-stage1/data/restaurants.json`;
+    return `https://amalkrishnan.me/mws-restaurant-stage1/data/restaurants.json`;
   }
 
   /**
