a448d417fa898f0daa2a6dbb3723c890e89d694c
Amal Krishnan <amal.aug95@gmail.com>
2018-07-17 14:25:24 +0530

    sw version increment

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
 
-const PRECACHE = 'precache-v1' ;
+const PRECACHE = 'precache-v2' ;
 const RUNTIME = 'runtime' ;
 
-// A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   '/',
   './index.html',
@@ -12,10 +11,9 @@ const PRECACHE_URLS = [
   './js/main.js',
   './js/restaurant_info.js',
   './data/restaurants.json',
-  './img/'
+  './img/*'
 ];
 
-// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(PRECACHE)
@@ -24,7 +22,6 @@ self.addEventListener('install', event => {
   );
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
   const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
@@ -38,11 +35,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
@@ -52,7 +45,6 @@ self.addEventListener('fetch', event => {
 
         return caches.open(RUNTIME).then(cache => {
           return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
               return response;
             });
