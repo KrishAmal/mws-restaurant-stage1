0c0bcc5c9e7039ba99c0571cadacf5be332a7af8
Amal Krishnan <amal.aug95@gmail.com>
2018-07-17 23:17:30 +0530

    suggested changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const PRECACHE = 'precache-v2' ;
-const RUNTIME = 'runtime' ;
+const PRECACHE = 'precache-v2';
+const RUNTIME = 'runtime';
 
 const PRECACHE_URLS = [
   '/',
@@ -10,7 +10,17 @@ const PRECACHE_URLS = [
   './js/dbhelper.js',
   './js/main.js',
   './js/restaurant_info.js',
-  './data/restaurants.json'
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg'
 ];
 
 self.addEventListener('install', event => {
@@ -35,19 +45,47 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
+      //Cloning the request
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(
+        function (response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // IMPORTANT: Clone the response. A response is a stream
+          // and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need
+          // to clone it so we have two streams.
+          var responseToCache = response.clone();
+
+          caches.open(PRECACHE)
+            .then(function (cache) {
+              cache.put(event.request, responseToCache);
             });
+
+          return response;
+        }
+      );
+
+
+      return caches.open(RUNTIME).then(cache => {
+        return fetch(event.request).then(response => {
+          return cache.put(event.request, response.clone()).then(() => {
+            return response;
           });
         });
-      })
-    );
+      });
+
+
+    })
+  );
 });
